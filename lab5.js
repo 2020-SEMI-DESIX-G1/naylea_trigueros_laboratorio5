7362af24b6ff4eb438032495c52966d0bed67189
vagrant <vagrant@debian9.localdomain>
2020-06-05 02:58:27 +0000

    archivos  e imagen agregados

---
+++ lab5.js
@@ -0,0 +1,31 @@
+var a = 5
+var b = 4
+suma = a +  b
+res = a - b
+multi = a * b
+divi = a/b
+console.log("Problema#1");
+console.log(suma ,res, multi, divi);
+
+console.log("Problema#2");
+let c =" Naylea";
+let d ="Trigueros";
+console.log(c + ' ' + d);
+
+console.log("Problema#3");
+const num = 9;
+const num2 = 10;
+console.log(typeof num, typeof num2);
+
+console.log("Problema#4");
+var objeto ={
+	entero: "1",
+        string: new String("hola"),
+        booleano: e = true,
+	vacio:[,],
+};
+
+console.log(objeto);
+
+
+
